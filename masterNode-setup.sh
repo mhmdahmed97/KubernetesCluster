c1dd255037f34e6bdcb87099ec846e2b712b97b8
Muhammed Ahmed <63552526+mhmdahmed97@users.noreply.github.com>
2022-05-06 17:42:46 +0500

    Update masterNode-setup.sh

--- masterNode-setup.sh
+++ masterNode-setup.sh
@@ -71,4 +71,8 @@ sudo kubeadm config images pull
 sudo kubeadm init \
   --pod-network-cidr=192.168.0.0/16
 
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 
